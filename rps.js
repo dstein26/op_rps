826c49b5acbce2d7519f80c6f969a9a6f35d7e57
D.Stein <rckstrongo@gmail.com>
2023-09-10 15:01:11 -0400

    Basic Buttons
    Added some js code to reacted to the user pushing a button

--- rps.js
+++ rps.js
@@ -1,1 +1,24 @@
 console.log("Welcome to rock paper scissors")
+
+const rock = document.getElementById("ROCK");
+const paper = document.getElementById("PAPER");
+const scissors = document.getElementById("SCISSORS")
+
+rock.addEventListener("click", selectRock);
+paper.addEventListener("click", selectPaper);
+scissors.addEventListener("click", selectScissors);
+
+function selectRock()
+{
+    console.log("User selected rock");
+}
+
+function selectPaper()
+{
+    console.log("User selected paper");
+}
+
+function selectScissors()
+{
+    console.log("User selected scissors");
+}
